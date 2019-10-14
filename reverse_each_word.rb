edbf3b13061d4acee20f6e4d6d2bb0bd77b69b93
Kevin Downs <kevin.downs04@gmail.com>
2019-10-14 22:13:08 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -9,3 +9,9 @@ def reverse_each_word(string)
 end
 
 def reverse_each_word(string)
+  st_array = string.split
+  r_array = st_array.collect do |element|
+    element.reverse
+  end
+  return r_array.join(" ")
+end
