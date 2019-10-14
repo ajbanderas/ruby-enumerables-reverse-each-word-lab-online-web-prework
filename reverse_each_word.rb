34a3beecf0bb94c6e0febdf4938ed8b0c298809e
Kevin Downs <kevin.downs04@gmail.com>
2019-10-14 21:27:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,8 @@
 
 def reverse_each_word(string)
   var array = string.split()
-  reverse_array = array.each do |reverse|
-    
+  reverse_array = array.each do |element|
+    element.reverse
+  end
+  puts reverse_array
 end
