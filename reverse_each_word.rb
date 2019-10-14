f4383cd547f65b2f4c71dac1fbd542914db1509c
Kevin Downs <kevin.downs04@gmail.com>
2019-10-14 21:56:27 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ def reverse_each_word(string)
   st_array = string.split
   r_array = []
   st_array.each do |element|
-    
+    r_array.push(element.reverse)
   end
-  puts st_array.join(" ")
+  puts r_array.join(" ")
 end
